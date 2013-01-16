93916f34ee3147a248784b8bd8f593e034ec1426
Michael Luskind <mluskind@gmail.com>
2013-01-16 18:55:46 -0500

    Updated Organizations - register_field_group

--- classes/class-organizations.php
+++ classes/class-organizations.php
@@ -1,4 +1,88 @@
 <?php
 class ScaleUp_Organizations {
-  
+
+  private $_args;
+
+  private $_post_type;
+
+  function __construct( $post_type, $args = array() ) {
+
+    $defaults = array(
+      'label' => 'Organizations',
+      'description' => '',
+      'public' => true,
+      'show_ui' => true,
+      'show_in_menu' => true,
+      'capability_type' => 'post',
+      'hierarchical' => false,
+      'rewrite' => array(
+        'slug' => ''
+      ),
+      'query_var' => true,
+      'exclude_from_search' => false,
+      'supports' => array(
+        'title',
+        'editor',
+        'excerpt',
+        'trackbacks',
+        'custom-fields',
+        'comments',
+        'revisions',
+        'thumbnail',
+        'author',
+        'page-attributes',
+      ),
+      'labels' => array(
+        'name' => 'Organizations',
+        'singular_name' => 'Organization',
+        'menu_name' => 'Organizations',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New Organization',
+        'edit' => 'Edit',
+        'edit_item' => 'Edit Organization',
+        'new_item' => 'New Organization',
+        'view' => 'View',
+        'view_item' => 'View Organization',
+        'search_items' => 'Search Organization',
+        'not_found' => 'No Organization Found ',
+        'not_found_in_trash' => 'Organization found in Trash',
+        'parent' => 'Parent Organization',
+      )
+    );
+
+    $this->_args      = wp_parse_args( $args, $defaults );
+    $this->_post_type = $post_type;
+
+    add_action( 'init', array( $this, 'init' ) );
+    add_action( 'register_schemas', array( $this, 'register_schemas' ) );
+
+  }
+
+  function init() {
+    register_post_type( $this->_post_type, $this->_args );
+  }
+
+  /**
+   * Register Organizations post type with schemas
+   */
+  function register_schemas() {
+
+    $default = array(
+      'table' => 'postmeta',
+      'default' => '',
+      'type' => 'text',
+      'create' => 'add_post_meta',
+      'read' => 'get_post_meta',
+      'update' => 'update_post_meta',
+      'delete' => 'delete_post_meta'
+    );
+
+    $fields     = array();
+    $properties = get_schema_fields( 'Organization', true );
+    foreach ( $properties as $property )
+      $fields[ $property ] = $default;
+
+    register_schema( 'Organization', $this->_post_type, $fields );
+  }
+
 }
--- classes/class-plugin-base.php
+++ classes/class-plugin-base.php
@@ -1,16 +1,98 @@
 <?php
 class ScaleUp_Organizations_Plugin {
 
-    private static $_this;
+  private static $_this;
 
-    public static function this() {
-        return self::$_this;
+  private static $_organizations;
 
+  private static $_organization_post_type;
+
+  public static function this() {
+    return self::$_this;
+
+
+  }
+
+  function __construct() {
+
+    self::$_this                   = $this;
+    self::$_organization_post_type = apply_filters( 'scaleup_organization_plugin_organization_post_type', 'scaleup_organization' );
+    self::$_organizations          = new ScaleUp_Organizations( self::$_organization_post_type );
+
+    add_action( 'admin_init', array( $this, 'admin_init' ) );
+
+  }
+
+  function admin_init() {
+    $this->register_field_group();
+  }
+
+  function register_field_group() {
+    $properties = ScaleUp_Schemas::get_schema_fields( 'Organization', true );
+    $post_type  = self::$_organization_post_type;
+
+    $i      = 0;
+    $fields = array();
+    foreach ( $properties as $property ) {
+      $reference = get_schema_field( $property, true );
+
+      $field = array(
+        'key'                 => "$post_type.$property",
+        'label'               => $reference[ 'label' ],
+        'name'                => "$post_type.$property",
+        'type'                => 'text',
+        'order_no'            => $i,
+        'instructions'        => $reference[ 'comment_plain' ],
+        'required'            => false,
+        'conditional_logic'   => array(
+          'status'   => 0,
+          'rules'    => array(
+            0 =>  array(
+              'field'    => 'null',
+              'operator' => '==',
+            ),
+          ),
+          'allorany' => 'all',
+        ),
+        'default_value'     => '',
+        'formatting'        => 'none',
+      );
+
+      $fields[ ] = $field;
+      $i++;
     }
 
-    function __construct( $args = array() ) {
-        self::$_this = $this;
+    $field_group = array(
+      'id' => 'scaleup_organization_fields',
+      'title' => 'Organization Fields',
+      'fields' => $fields,
+      'location' =>
+      array(
+        'rules' =>
+        array(
+          0 =>
+          array(
+            'param' => 'post_type',
+            'operator' => '==',
+            'value' => $post_type,
+            'order_no' => 0,
+          ),
+        ),
+        'allorany' => 'all',
+      ),
+      'options' =>
+      array(
+        'position' => 'normal',
+        'layout' => 'no_box',
+        'hide_on_screen' =>
+        array(),
+      ),
+      'menu_order' => 0,
+    );
+    register_field_group( $field_group );
+  }
 
-    }
 
 }
+
+new ScaleUp_Organizations_Plugin();
